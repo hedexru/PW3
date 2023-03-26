fa4a845eb8cff57054f8580a8ecd5adb84e22bd6
hedexru <122992238+hedexru@users.noreply.github.com>
2023-03-27 01:23:07 +0300

    Update Program.cs

--- PW3_4/Program.cs
+++ PW3_4/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace PW3_4
+namespace PW3_4
 {
     internal class Program
     {
+        class MyIntList
+        {
+            private List<int> numbersList = new List<int>();
+
+            public double Average
+            {
+                get
+                {
+                    return CalculateAverage();
+                }
+            }
+
+            public void AddNumber(int number)
+            {
+                numbersList.Add(TranslateNumber());
+            }
+
+            public void RemoveNumber(int number)
+            {
+                numbersList.Remove(number);
+            }
+
+
+            public double CalculateAverage()
+            {
+                int sum = 0;
+                foreach (int number in numbersList)
+                {
+                    sum += number;
+                }
+                Console.WriteLine(sum/(double)numbersList.Count);
+                return sum / (double)numbersList.Count;
+            }
+            public int TranslateNumber()
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                return number;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyIntList myIntList = new MyIntList();
+            myIntList.AddNumber(myIntList.TranslateNumber());
+            myIntList.AddNumber(myIntList.TranslateNumber());
+            myIntList.AddNumber(myIntList.TranslateNumber());
+            myIntList.CalculateAverage();
         }
     }
 }
