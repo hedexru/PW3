904c90e67b24d43b45fb848af61bdd4339a8d3d9
HeDeX <SergeyProskutinRU@gmail.com>
2023-03-27 00:02:30 +0300

    v1

--- PW3_1/Program.cs
+++ PW3_1/Program.cs
@@ -2,9 +2,70 @@
 {
     internal class Program
     {
+        class Color
+        {
+            private int red;
+            private int green;
+            private int blue;
+
+            public int Red
+            {
+                get { return red; }
+                set { red = NormalizeColorValue(value); }
+            }
+
+            public int Green
+            {
+                get { return red; }
+                set { green = NormalizeColorValue(value); }
+            }
+
+            public int Blue
+            {
+                get { return red; }
+                set { blue = NormalizeColorValue(value); }
+            }
+
+            public Color() { }
+
+            public Color(int red, int green, int blue)
+            {
+                Red = red;
+                Green = green;
+                Blue = blue;
+            }
+
+            public void DisplayColor()
+            {
+                Console.WriteLine($"{red}, {green}, {blue}");
+            }
+
+            private int NormalizeColorValue(int colorValue)
+            {
+                if (colorValue > 255)
+                {
+                    return 255;
+                }
+                if (colorValue < 0)
+                {
+                    return 0;
+                }
+                return colorValue;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Color yellow = new Color(300, 300, 0);
+            yellow.DisplayColor();
+
+            Color pink = new Color();
+            pink.Red = 255;
+            pink.Green = -20;
+            pink.Blue = 147;
+
+            pink.DisplayColor();
+
+            Console.ReadKey(true);
         }
     }
 }
--- PW3_2/Program.cs
+++ PW3_2/Program.cs
@@ -2,9 +2,67 @@
 {
     internal class Program
     {
+        public class SmsMessage
+        {
+            private const int MaxSMSLength = 250;
+            private const int MaxTaxedLength = 65;
+            private const decimal BasePrice = 1.5m;
+            private const decimal TaxPrice = 0.5m;
+
+            private string messageText;
+            private decimal price;
+
+            public SmsMessage(string messageText)
+            {
+                MessageText = messageText;
+                Price = CalculatePrice(messageText);
+            }
+
+            public string MessageText
+            {
+                get { return messageText; }
+                set { messageText = value.Substring(0, Math.Min(value.Length, MaxSMSLength)); }
+            }
+
+            public decimal Price
+            {
+                get { return price; }
+                set { price = value; }
+            }
+
+            public static SmsMessage CreateFromUserInput()
+            {
+                Console.WriteLine("Введите текст сообщения:");
+                string messageText = Console.ReadLine();
+                return new SmsMessage(messageText);
+            }
+
+            private static decimal CalculatePrice(string messageText)
+            {
+                int length = Math.Min(messageText.Length, MaxSMSLength);
+                decimal price = BasePrice;
+
+                if (length > MaxTaxedLength)
+                {
+                    int taxedLength = length - MaxTaxedLength;
+                    price += taxedLength * TaxPrice;
+                }
+
+                return price;
+            }
+            public void ReturnValues()
+            {
+                Console.WriteLine(Price);
+                Console.WriteLine(messageText);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SmsMessage otkritka = new SmsMessage("toster");
+            otkritka.ReturnValues();
+
+            SmsMessage bigmessage = new SmsMessage("Very big sms message idk what i need to write here but i write smth interesting but not here because here big nothing meaning text");
+            bigmessage.ReturnValues();
         }
     }
 }
